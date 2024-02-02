510bc679a7e58e811b219923d6f15a2bf4426100
Preeti-46 <158249873+Preeti-46@users.noreply.github.com>
2024-02-02 12:46:21 +0530

    Update Others.js

--- src/pages/Others.js
+++ src/pages/Others.js
@@ -53,14 +53,14 @@ export default function Others() {
           <motion.div variants={{ hidden: { opacity: 0, y: 75 }, visible: { opacity: "1", y: 0 } }} initial="hidden" animate="visible" transition={{ duration: 0.5, delay: 0.25 }} className='hosetl-imagee-1' style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "19px", fontSize: "1.3rem" }}>
             <img src={yuva} alt='adminblock' ></img>
 
-            <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", gap: "3px" }} className='o-y'> <p style={{ color: "#4c5157" }}>Visit Of Karnataka team to IIT</p><p>Ropar.</p></div>
+            <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", gap: "3px" }} className='o-y'> <p style={{ color: "#4c5157" }}>Visit of Karnataka team to IIT</p><p>Ropar.</p></div>
 
 
           </motion.div>
           <motion.div variants={{ hidden: { opacity: 0, y: 75 }, visible: { opacity: "1", y: 0 } }} initial="hidden" animate="visible" transition={{ duration: 0.5, delay: 0.25 }} className='hosetl-imagee-1' style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "10px", fontSize: "1.3rem" }}>
             <img src={yuva1} alt='adminblock' ></img>
-            <div style={{ display: "flex", flexWrap: "wrap", gap: "3px", justifyContent: "center" }} className='o-y'> <p style={{ color: "#4c5157" }}>Visit of Punjab students and</p>
-              <p style={{ color: "#4c5157" }}>IIT Ropar to Dharwad, Karnataka.</p></div>
+            <div style={{ display: "flex", flexWrap: "wrap", gap: "3px", justifyContent: "center" }} className='o-y'> <p style={{ color: "#4c5157" }}>Visit of Punjab team</p>
+              <p style={{ color: "#4c5157" }}>to IIT Dharwad.</p></div>
           </motion.div>
         </div>
       </motion.div>
